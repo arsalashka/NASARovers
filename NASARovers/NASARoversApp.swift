975e2864884b278a68e93146e8813a9f200514aa
Arsalan Balzhinimaev <arsalashka@gmail.com>
2024-07-03 15:46:32 -0400

    Initial commit

---
+++ NASARovers/NASARoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASARoversApp.swift
+//  NASARovers
+//
+//  Created by Arsalan on 03.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NASARoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
